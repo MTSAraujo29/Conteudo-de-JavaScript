f9f81c8c507b328a11cf7fd355c25c9a410803b1
Matheus Ferreira De Araujo <matheusferreiradearaujo19@gmail.com>
2022-06-23 14:10:19 -0300

    js

--- curso js/exe9/exe9.js
+++ curso js/exe9/exe9.js
@@ -1,1 +1,6 @@
-console.log('o console funcio')
+var vel = 40
+console.log(`A velocidade do seu carro e ${vel} km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Voce ultrapasou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
