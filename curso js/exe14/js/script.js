85aebb51f51017fbf6a376ccedb84359b35190a0
Matheus Ferreira De Araujo <matheusferreiradearaujo19@gmail.com>
2022-06-25 10:48:37 -0300

    atu

--- curso js/exe14/js/script.js
+++ curso js/exe14/js/script.js
@@ -1,21 +1,23 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('img')
+    var img = window.document.getElementById('foto')
     var data = new Date()
     var hora = data.getHours()
 
-    msg.innerHTML = `Agora são <strong>${hora}</strong> horas`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
         img.src = 'm.png'
-        window.document.body.style.background = 'rgba(247, 231, 4, 0.452)'
+        window.document.body.style.background = 'rgba(228, 218, 76, 0.808)'
+        msg.innerHTML = `Agora são ${hora} horas <strong>BOM DIA!</strong>`
     } else if (hora >= 12 && hora <= 18) {
         //BOA TARDE!
         img.src = 't.png'
         window.document.body.style.background = 'rgba(230, 163, 39, 0.774)'
+        msg.innerHTML = `Agora são ${hora} horas <strong>BOA TARDE!</strong>`
     } else {
         //BOA NOITE!
         img.src = 'n.png'
         window.document.body.style.background = 'rgba(5, 7, 119, 0.822)'
+        msg.innerHTML = ` Agora são ${hora} horas <strong>BOA NOITE!</strong>`
     }
 }
