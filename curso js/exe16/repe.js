f15730b7afdde8aa80f3e9e236aab48387c6b10c
Matheus Ferreira De Araujo <matheusferreiradearaujo19@gmail.com>
2022-06-28 11:33:25 -0300

    oi

---
+++ curso js/exe16/repe.js
@@ -0,0 +1,20 @@
+/*
+    Na repetição exitir sequencias
+    e as vezes vai exitir condiçoes diferentes
+    N a repetição o objetivo
+    ele fica em loop ate o resultado for true
+    quando for false ele e quebrado
+    e vai para o destino final
+
+    Pra acontecer esse loop quando for true
+    e necessario usar a palavra 
+    while  mais a (condição) dentro de um
+    bloco {}, toda vez que tiver a palavra
+    while ler como ENQUANTO 
+*/
+
+var c = 2
+while (c <= 25) {
+    console.log('Tudo bem?')
+    c++
+}
